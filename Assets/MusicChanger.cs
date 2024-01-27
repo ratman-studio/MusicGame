28b82aef1256ac13a79fc20bafbf3db1aa05a375
Hubert Niećko <hubert.niecko@boombit.com>
2024-01-27 23:02:34 +0100

    A - music changer

---
+++ Assets/MusicChanger.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FMODUnity;
+using UnityEngine;
+
+public class MusicChanger : MonoBehaviour
+{
+    [SerializeField] private StudioGlobalParameterTrigger PlayerSpeed;
+    [SerializeField] private StudioGlobalParameterTrigger Mistake;
+
+    [SerializeField] private GroundedCharacterController _characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        InSceneLevelSwitcher.OnLevelStart += StartMusic;
+    }
+
+    private void OnDisable()
+    {
+        InSceneLevelSwitcher.OnLevelStart -= StartMusic;
+    }
+
+    private void StartMusic()
+    {
+        GlobalTrigger.TriggerEvent()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BoostLevel(float boostLevel)
+    {
+        throw new System.NotImplementedException();
+    }
+}
